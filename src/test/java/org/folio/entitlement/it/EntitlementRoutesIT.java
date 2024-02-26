ef5315a9eccbb9a27a6e82a4f60ec6075a4901b8
okuzminov <oleksii_kuzminov@epam.com>
2024-02-26 08:15:18 +0100

    fix tests

--- src/test/java/org/folio/entitlement/it/EntitlementRoutesIT.java
+++ src/test/java/org/folio/entitlement/it/EntitlementRoutesIT.java
@@ -5,6 +5,7 @@ import static java.util.Collections.emptyMap;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.awaitility.Durations.FIVE_SECONDS;
+import static org.awaitility.Durations.TEN_SECONDS;
 import static org.awaitility.Durations.TWO_HUNDRED_MILLISECONDS;
 import static org.folio.common.utils.OkapiHeaders.MODULE_ID;
 import static org.folio.common.utils.OkapiHeaders.TENANT;
@@ -124,9 +125,10 @@ class EntitlementRoutesIT extends BaseIntegrationTest {
   @MethodSource("multipleRoutesDataProvider")
   @DisplayName("[2] checkInstalledRoutes_positive_parameterizedForTypeMultiple")
   @WireMockStub("/wiremock/application-routes-it.json")
+  @ParameterizedTest(name = "[{index}] method={0}, uri={1}")
   void checkInstalledRoutes_positive_parameterizedForTypeMultiple(
     HttpMethod method, URI uri, String moduleId, WiremockResponse resp) {
-    await().atMost(FIVE_SECONDS)
+    await().atMost(TEN_SECONDS)
       .pollInterval(TWO_HUNDRED_MILLISECONDS)
       .untilAsserted(() -> {
         var result = HTTP_CLIENT.send(httpRequest(method, uri, TENANT_NAME, moduleId), ofString());
