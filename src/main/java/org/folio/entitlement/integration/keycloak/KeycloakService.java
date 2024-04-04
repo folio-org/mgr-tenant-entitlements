148c49b0cad80163c72bdbbd19dc4648ab11578b
Pavel Filippov <pavel_filippov@epam.com>
2024-04-04 15:36:47 +0300

    MGRENTITLE-19 Implement upgrade operation for Keycloak service

--- src/main/java/org/folio/entitlement/integration/keycloak/KeycloakService.java
+++ src/main/java/org/folio/entitlement/integration/keycloak/KeycloakService.java
@@ -2,25 +2,34 @@ package org.folio.entitlement.integration.keycloak;
 
 import static java.lang.String.format;
 import static java.util.Arrays.stream;
+import static java.util.Collections.emptyMap;
+import static java.util.function.Predicate.not;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
 import static org.apache.http.HttpStatus.SC_CONFLICT;
 import static org.apache.http.HttpStatus.SC_NOT_FOUND;
+import static org.folio.common.utils.CollectionUtils.toStream;
+import static org.folio.common.utils.Collectors.toLinkedHashMap;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.ws.rs.ClientErrorException;
-import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.WebApplicationException;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.folio.common.domain.model.ModuleDescriptor;
 import org.folio.common.domain.model.error.Parameter;
 import org.folio.entitlement.integration.IntegrationException;
 import org.folio.entitlement.integration.keycloak.configuration.properties.KeycloakConfigurationProperties;
-import org.folio.security.integration.keycloak.model.KeycloakMappings;
 import org.folio.security.integration.keycloak.service.KeycloakModuleDescriptorMapper;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.AuthorizationResource;
@@ -36,123 +45,235 @@ public class KeycloakService {
   private final KeycloakModuleDescriptorMapper moduleDescriptorMapper;
   private final KeycloakConfigurationProperties properties;
 
-  public void registerModuleResources(ModuleDescriptor moduleDescriptor, String realmName) {
-    var mappings = moduleDescriptorMapper.map(moduleDescriptor, true);
-    var clientId = getClientId(realmName);
-    var resources = mappings.getResourceServer().getResources();
-
-    createScopes(clientId, realmName);
-    createResources(clientId, realmName, resources);
-  }
-
-  public void unregisterModuleResources(ModuleDescriptor moduleDescriptor, String realmName) {
-    var mappings = moduleDescriptorMapper.map(moduleDescriptor, true);
-    var clientId = getClientId(realmName);
-
-    removeResources(clientId, realmName, mappings);
-  }
-
-  private String getClientId(String realmName) {
-    var loginClientId = realmName + properties.getLogin().getClientNameSuffix();
-    var loginClients = keycloakClient.realm(realmName).clients().findByClientId(loginClientId);
-    var client = loginClients.stream()
-      .filter(cl -> cl.getClientId().equals(loginClientId))
-      .findFirst();
-
-    if (client.isEmpty()) {
-      throw new EntityNotFoundException("Client for login operations was not found. Client name:" + loginClientId);
+  /**
+   * Registers authorization resources and scopes in Keycloak.
+   *
+   * @param prevDescriptor - previous module descriptor (if upgrade), nullable
+   * @param newDescriptor - new module descriptor, must be not-null
+   * @param realm - Keycloak realm name
+   */
+  public void updateAuthResources(ModuleDescriptor prevDescriptor, ModuleDescriptor newDescriptor, String realm) {
+    var authResourceClient = getAuthorizationClient(realm);
+    var scopeErrorParameters = createScopes(authResourceClient);
+    if (isNotEmpty(scopeErrorParameters)) {
+      throw new IntegrationException("Failed to update authorization scopes in Keycloak", scopeErrorParameters);
     }
-    return client.get().getId();
-  }
 
-  private void createScopes(String clientId, String realmName) {
-    var authzClient = getAuthorizationResource(clientId, realmName);
-    var existingScopes = authzClient.scopes().scopes()
-      .stream()
-      .map(ScopeRepresentation::getName)
+    var newAuthResources = getAuthorizationResources(newDescriptor);
+    var prevAuthResources = getAuthorizationResources(prevDescriptor);
+
+    var newResources = newAuthResources.entrySet().stream()
+      .filter(not(entry -> resourceAlreadyExists(entry.getKey(), entry.getValue(), prevAuthResources)))
+      .map(Entry::getValue)
       .toList();
 
-    stream(HttpMethod.values())
-      .map(HttpMethod::name)
-      .filter(method -> !existingScopes.contains(method))
-      .map(methodName -> {
-        var scope = new ScopeRepresentation();
-        scope.setName(methodName);
-        return scope;
-      })
-      .forEach(scopeRepresentation -> createScopeIgnoreConflict(authzClient, scopeRepresentation));
+    var deprecatedResources = prevAuthResources.keySet().stream()
+      .filter(not(newAuthResources::containsKey))
+      .map(prevAuthResources::get)
+      .toList();
+
+    var resourceErrorParameters = new ArrayList<Parameter>();
+    resourceErrorParameters.addAll(createResources(authResourceClient, newResources));
+    resourceErrorParameters.addAll(removeResources(authResourceClient, deprecatedResources));
+
+    if (isNotEmpty(resourceErrorParameters)) {
+      throw new IntegrationException("Failed to update authorization resources in Keycloak", resourceErrorParameters);
+    }
   }
 
-  private void createScopeIgnoreConflict(AuthorizationResource client, ScopeRepresentation scope) {
+  /**
+   * Removes authorization resources from Keycloak.
+   *
+   * @param moduleDescriptor - previous module descriptor (if upgrade), nullable
+   * @param realmName - Keycloak realm name
+   */
+  public void removeAuthResources(ModuleDescriptor moduleDescriptor, String realmName) {
+    var authResourceClient = getAuthorizationClient(realmName);
+    var mappings = moduleDescriptorMapper.map(moduleDescriptor, true);
+    var resources = mappings.getResourceServer().getResources();
+
+    var removeResourcesErrors = removeResources(authResourceClient, resources);
+    if (isNotEmpty(removeResourcesErrors)) {
+      throw new IntegrationException("Failed to remove authorization resources in Keycloak", removeResourcesErrors);
+    }
+  }
+
+  private static boolean resourceAlreadyExists(String resourceName, ResourceRepresentation resource,
+    Map<String, ResourceRepresentation> prevAuthResources) {
+    var prevResource = prevAuthResources.get(resourceName);
+    return prevAuthResources.containsKey(resourceName) && Objects.equals(getScopes(resource), getScopes(prevResource));
+  }
+
+  private static Set<String> getScopes(ResourceRepresentation resourceRepresentation) {
+    return toStream(resourceRepresentation.getScopes()).map(ScopeRepresentation::getName).collect(toSet());
+  }
+
+  private Map<String, ResourceRepresentation> getAuthorizationResources(ModuleDescriptor moduleDescriptor) {
+    if (moduleDescriptor == null) {
+      return emptyMap();
+    }
+
+    var mappings = moduleDescriptorMapper.map(moduleDescriptor, true);
+    var resources = mappings.getResourceServer().getResources();
+    return groupResourcesByName(resources);
+  }
+
+  private static Map<String, ResourceRepresentation> groupResourcesByName(List<ResourceRepresentation> resources) {
+    return resources.stream().collect(toLinkedHashMap(ResourceRepresentation::getName));
+  }
+
+  private AuthorizationResource getAuthorizationClient(String realmName) {
+    var name = realmName + properties.getLogin().getClientNameSuffix();
+    var loginClients = keycloakClient.realm(realmName).clients().findByClientId(name);
+    var loginClientId = loginClients.stream()
+      .filter(cl -> cl.getClientId().equals(name))
+      .findFirst()
+      .orElseThrow(() -> new EntityNotFoundException("Client for login operations was not found by name: " + name))
+      .getId();
+
+    return getAuthorizationResourceClient(loginClientId, realmName);
+  }
+
+  private List<Parameter> createScopes(AuthorizationResource client) {
+    var existingScopes = getAuthorizationScopes(client);
+
+    return stream(HttpMethod.values())
+      .map(HttpMethod::name)
+      .filter(method -> !existingScopes.containsKey(method))
+      .map(KeycloakService::mapToScopeRepresentation)
+      .map(scopeRepresentation -> createScopeIgnoreConflict(client, scopeRepresentation))
+      .flatMap(Optional::stream)
+      .toList();
+  }
+
+  private static ScopeRepresentation mapToScopeRepresentation(String methodName) {
+    var scope = new ScopeRepresentation();
+    scope.setName(methodName);
+    return scope;
+  }
+
+  private Optional<Parameter> createScopeIgnoreConflict(AuthorizationResource client, ScopeRepresentation scope) {
     var scopeName = scope.getName();
     try (var response = client.scopes().create(scope)) {
-      validateResponseIgnoringConflict(response, () -> "Error during creating scope in Keycloak: " + scopeName);
+      var status = response.getStatus();
+      if (status >= SC_BAD_REQUEST && status != SC_CONFLICT) {
+        return Optional.of(new Parameter().key("scope").value(format(
+          "Failed to create scope %s (status: %s, info: %s)", scope.getName(), status, response.getStatusInfo())));
+      }
+
       log.debug("Scope was created with name: #{}", scopeName);
     }
+
+    return Optional.empty();
   }
 
-  private void createResources(String clientId, String realmName, Collection<ResourceRepresentation> resources) {
-    var authzClient = getAuthorizationResource(clientId, realmName);
-    var scopes = authzClient.scopes().scopes()
-      .stream().collect(Collectors.toMap(ScopeRepresentation::getName, ScopeRepresentation::getId));
+  private static List<Parameter> createResources(AuthorizationResource client,
+    Collection<ResourceRepresentation> resources) {
+    var scopes = getAuthorizationScopes(client);
 
-    resources.forEach(resource -> {
-      resource.getScopes().forEach(scope -> scope.setId(scopes.get(scope.getName())));
-      createResourceIgnoreConflict(authzClient, resource);
-    });
+    return resources.stream()
+      .map(resourceRepresentation -> withScopeIds(resourceRepresentation, scopes))
+      .map(resource -> createResourceSafe(client, resource))
+      .flatMap(Optional::stream)
+      .toList();
   }
 
-  private void createResourceIgnoreConflict(AuthorizationResource client, ResourceRepresentation resource) {
-    var resourceName = resource.getName();
+  private static Map<String, String> getAuthorizationScopes(AuthorizationResource authResourceClient) {
+    return toStream(authResourceClient.scopes().scopes())
+      .collect(toMap(ScopeRepresentation::getName, ScopeRepresentation::getId, (o1, o2) -> o1));
+  }
+
+  private static Optional<Parameter> createResourceSafe(AuthorizationResource client, ResourceRepresentation resource) {
+    var name = resource.getName();
     try (var response = client.resources().create(resource)) {
-      validateResponseIgnoringConflict(response, () -> "Error during creating resource in Keycloak: " + resourceName);
-      log.debug("Resource was created with name: #{}", resourceName);
+      var status = response.getStatus();
+      if (status == SC_CONFLICT) {
+        return updateResource(client, resource);
+      }
+
+      if (status >= SC_BAD_REQUEST) {
+        return Optional.of(new Parameter().key("resource").value(format(
+          "Failed to create resource: %s (status: %s, info: %s)", name, status, response.getStatusInfo())));
+      }
+
+      log.debug("Resource was created with name: #{}", name);
+    }
+
+    return Optional.empty();
+  }
+
+  private static Optional<Parameter> updateResource(AuthorizationResource client, ResourceRepresentation resource) {
+    var name = resource.getName();
+    var resourceByName = findResourceByName(client, name);
+    if (resourceByName.isEmpty()) {
+      return Optional.of(new Parameter().key("resource").value("Failed to find created resource by name: " + name));
+    }
+
+    var resourceRepresentation = resourceByName.get();
+    var resourceId = resourceRepresentation.getId();
+    resourceRepresentation.setScopes(resource.getScopes());
+
+    try {
+      client.resources().resource(resourceId).update(resourceRepresentation);
+      log.debug("Authorization scopes are updated for resource: name = {}", name);
+      return Optional.empty();
+    } catch (WebApplicationException exception) {
+      var param = new Parameter().key("resource").value(String.format(
+        "Failed to update resource: %s (status: %s, causeMessage: %s)",
+        name, exception.getResponse().getStatus(), exception.getMessage()));
+      return Optional.of(param);
     }
   }
 
-  private void removeResources(String clientId, String realmName, KeycloakMappings mappings) {
-    var resources = mappings.getResourceServer().getResources();
-    var authzClient = getAuthorizationResource(clientId, realmName);
-
-    resources.forEach(resource -> removeResourceIfExist(authzClient, resource));
+  private static ResourceRepresentation withScopeIds(ResourceRepresentation resource, Map<String, String> scopes) {
+    resource.getScopes().forEach(scope -> scope.setId(scopes.get(scope.getName())));
+    return resource;
   }
 
-  private void removeResourceIfExist(AuthorizationResource client, ResourceRepresentation resource) {
-    try {
-      var searchResourcesResult = client.resources().findByName(resource.getName());
-      var existingResource = searchResourcesResult.stream()
-        .filter(res -> res.getName().equals(resource.getName()))
-        .findFirst();
+  private static List<Parameter> removeResources(AuthorizationResource authResourceClient,
+    List<ResourceRepresentation> resources) {
+    return resources.stream()
+      .map(resource -> removeResourceIfExist(authResourceClient, resource))
+      .flatMap(Optional::stream)
+      .toList();
+  }
 
+  private static Optional<Parameter> removeResourceIfExist(AuthorizationResource client,
+    ResourceRepresentation resource) {
+    var resourceName = resource.getName();
+    try {
+      var existingResource = findResourceByName(client, resourceName);
       if (existingResource.isPresent()) {
         var id = existingResource.get().getId();
+        log.debug("Keycloak resource removed: id = {}, name = {}", id, resourceName);
         client.resources().resource(id).remove();
       }
-    } catch (ClientErrorException exception) {
-      if (exception.getResponse().getStatus() != SC_NOT_FOUND) {
-        throw new IntegrationException("Error during deleting resources in Keycloak", exception);
+    } catch (WebApplicationException exception) {
+      var response = exception.getResponse();
+      if (response.getStatus() != SC_NOT_FOUND) {
+        var value = String.format("Failed to delete resource: %s (status: %s, causeMessage: %s)",
+          resourceName, response.getStatusInfo(), exception.getMessage());
+        return Optional.of(new Parameter().key("resource").value(value));
       }
     }
+
+    return Optional.empty();
   }
 
-  private AuthorizationResource getAuthorizationResource(String clientId, String realmName) {
+  private static Optional<ResourceRepresentation> findResourceByName(AuthorizationResource client, String name) {
+    var searchResourcesResult = client.resources().findByName(name);
+    return searchResourcesResult.stream()
+      .filter(res -> res.getName().equals(name))
+      .findFirst();
+  }
+
+  private AuthorizationResource getAuthorizationResourceClient(String clientId, String realmName) {
     return keycloakClient.proxy(AuthorizationResource.class, authorizationResourceUri(clientId, realmName));
   }
 
   private URI authorizationResourceUri(String clientId, String realmName) {
     return URI.create(
       properties.getUrl() + "/admin/realms/" + realmName + "/clients/" + clientId + "/authz/resource-server");
-  }
-
-  private static void validateResponseIgnoringConflict(Response response, Supplier<String> errorMsgSupplier) {
-    var status = response.getStatus();
-    if (status >= SC_BAD_REQUEST && status != SC_CONFLICT) {
-      var msg = errorMsgSupplier.get();
-      var cause = new Parameter()
-        .key("cause")
-        .value(format("%s: %s", status, response.getStatusInfo()));
-      throw new IntegrationException(msg, List.of(cause));
-    }
   }
 }
 
