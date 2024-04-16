d4e402b35905d96c8e21d6424b5ab84b0b80ef6a
Pavel Filippov <pavel_filippov@epam.com>
2024-04-16 18:45:39 +0300

    MGRENTITLE-39 Add documentation for FlowFactory

--- src/main/java/org/folio/entitlement/service/flow/FlowFactory.java
+++ src/main/java/org/folio/entitlement/service/flow/FlowFactory.java
@@ -5,5 +5,11 @@ import org.folio.flow.api.Flow;
 
 public interface FlowFactory {
 
+  /**
+   * Creates entitle/upgrade/revoke flow from entitlement request.
+   *
+   * @param request - entitlement request
+   * @return created {@link Flow} to execute
+   */
   Flow createFlow(EntitlementRequest request);
 }
