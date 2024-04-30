15feddc34a7a633751b7874c2f077ee981e4bda9
Yaroslav_Kiriak <Yaroslav_Kiriak@epam.com>
2024-04-30 17:07:16 +0300

    MGRENTITLE-48: add HTTPS access to mgr-tenants

---
+++ src/main/java/org/folio/entitlement/integration/tm/TenantManagerClientConfiguration.java
@@ -0,0 +1,14 @@
+package org.folio.entitlement.integration.tm;
+
+import static org.folio.common.utils.FeignClientTlsUtils.getOkHttpClient;
+
+import feign.okhttp.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+
+public class TenantManagerClientConfiguration {
+
+  @Bean
+  public OkHttpClient feignClient(okhttp3.OkHttpClient okHttpClient, TenantManagerProperties properties) {
+    return getOkHttpClient(okHttpClient, properties.getTls());
+  }
+}
