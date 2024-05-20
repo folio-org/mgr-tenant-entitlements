b5c3e9956817f9bd2eb326c63105e2da18a8a9d3
yaroslav-kiriak <159446370+yaroslav-kiriak@users.noreply.github.com>
2024-05-20 16:30:18 +0300

    MGRENTITLE-52: add HTTPS access to application-manager (#87)
    Co-authored-by: Yaroslav_Kiriak <Yaroslav_Kiriak@epam.com>

--- src/main/java/org/folio/entitlement/integration/am/AppManagerClientConfiguration.java
+++ src/main/java/org/folio/entitlement/integration/am/AppManagerClientConfiguration.java
@@ -1,11 +1,14 @@
 package org.folio.entitlement.integration.am;
 
+import static org.folio.common.utils.FeignClientTlsUtils.buildTargetFeignClient;
+
 import feign.Contract;
-import feign.Feign;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
+import okhttp3.OkHttpClient;
+import org.folio.common.configuration.properties.TlsProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -23,10 +26,11 @@ public class AppManagerClientConfiguration {
 
   private String url;
 
+  private TlsProperties tls;
+
   @Bean
-  public ApplicationManagerClient applicationManagerClient(Contract contract, Encoder encoder, Decoder decoder) {
-    return Feign.builder()
-      .contract(contract).encoder(encoder).decoder(decoder)
-      .target(ApplicationManagerClient.class, url);
+  public ApplicationManagerClient applicationManagerClient(OkHttpClient okHttpClient, Contract contract,
+    Encoder encoder, Decoder decoder) {
+    return buildTargetFeignClient(okHttpClient, contract, encoder, decoder, tls, url, ApplicationManagerClient.class);
   }
 }
