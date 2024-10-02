322f9f1b4e6a0d696428da7612756abcd804f731
Mykola Makhin <mmakhin@ebsco.com>
2024-10-02 18:52:17 +0300

    MGRENTITLE-72: Additional tests for module calls retries.

--- src/main/java/org/folio/entitlement/configuration/RetryConfigurationProperties.java
+++ src/main/java/org/folio/entitlement/configuration/RetryConfigurationProperties.java
@@ -1,30 +1,52 @@
 package org.folio.entitlement.configuration;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Validated
 @Component
 @ConfigurationProperties(prefix = "retries")
 public class RetryConfigurationProperties {
 
-  private RetryConfigProps module;
-  private RetryConfigProps keycloak;
-  private RetryConfigProps kong;
+  @Builder.Default
+  private RetryConfigProps module = new RetryConfigProps();
+  @Builder.Default
+  private RetryConfigProps keycloak = new RetryConfigProps();
+  @Builder.Default
+  private RetryConfigProps kong = new RetryConfigProps();
 
   @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
   public static class RetryConfigProps {
+
+    @Builder.Default
     private int max = 3;
-    private RetryBackoffConfigProps backoff;
+    @Builder.Default
+    private RetryBackoffConfigProps backoff = new RetryBackoffConfigProps();
   }
 
   @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
   public static class RetryBackoffConfigProps {
+
+    @Builder.Default
     private int delay = 1000;
+    @Builder.Default
     private int maxdelay = 30000;
+    @Builder.Default
     private int multiplier = 5;
   }
 }
---
+++ src/test/java/org/folio/entitlement/integration/folio/stage/FolioModuleCallsRetriesTest.java
@@ -0,0 +1,149 @@
+package org.folio.entitlement.integration.folio.stage;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.folio.entitlement.domain.model.CommonStageContext.PARAM_REQUEST;
+import static org.folio.entitlement.domain.model.ModuleStageContext.PARAM_MODULE_DESCRIPTOR;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+import java.util.Map;
+import org.folio.common.domain.model.ModuleDescriptor;
+import org.folio.entitlement.configuration.RetryConfiguration;
+import org.folio.entitlement.configuration.RetryConfigurationProperties;
+import org.folio.entitlement.configuration.RetryConfigurationProperties.RetryBackoffConfigProps;
+import org.folio.entitlement.configuration.RetryConfigurationProperties.RetryConfigProps;
+import org.folio.entitlement.domain.model.EntitlementRequest;
+import org.folio.entitlement.domain.model.ModuleStageContext;
+import org.folio.entitlement.integration.IntegrationException;
+import org.folio.entitlement.integration.folio.FolioModuleService;
+import org.folio.entitlement.repository.FlowStageRepository;
+import org.folio.entitlement.service.EntitlementModuleService;
+import org.folio.flow.impl.StageContextImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@EnableRetry
+@ExtendWith(MockitoExtension.class)
+@Import(RetryConfiguration.class)
+@ContextConfiguration(classes = FolioModuleCallsRetriesTest.TestConfig.class)
+class FolioModuleCallsRetriesTest {
+
+  @Autowired private FolioModuleService folioModuleService;
+
+  @Autowired private FolioModuleInstaller folioModuleInstaller;
+
+  @Autowired private FolioModuleUninstaller folioModuleUninstaller;
+
+  @Autowired private FolioModuleUpdater folioModuleUpdater;
+
+  @AfterEach
+  void resetMocks() {
+    reset(folioModuleService);
+  }
+
+  @Test
+  void testRetryOnInstallerExecute() {
+    doThrow(new IntegrationException("Test", List.of(), 400)).when(folioModuleService).enable(any());
+    var moduleStageContext = new ModuleStageContext(new StageContextImpl("test",
+      Map.of(PARAM_REQUEST, EntitlementRequest.builder().build(), PARAM_MODULE_DESCRIPTOR, new ModuleDescriptor()),
+      Map.of()));
+    assertThatThrownBy(() -> folioModuleInstaller.execute(moduleStageContext)).isInstanceOf(IntegrationException.class);
+    verify(folioModuleService, times(3)).enable(any());
+  }
+
+  @Test
+  void testRetryOnInstallerCancel() {
+    doThrow(new IntegrationException("Test", List.of(), 400)).when(folioModuleService).disable(any());
+    var moduleStageContext = new ModuleStageContext(new StageContextImpl("test",
+      Map.of(PARAM_REQUEST, EntitlementRequest.builder().build(), PARAM_MODULE_DESCRIPTOR, new ModuleDescriptor()),
+      Map.of()));
+    assertThatThrownBy(() -> folioModuleInstaller.cancel(moduleStageContext)).isInstanceOf(IntegrationException.class);
+    verify(folioModuleService, times(3)).disable(any());
+  }
+
+  @Test
+  void testRetryOnUpdaterExecute() {
+    doThrow(new IntegrationException("Test", List.of(), 400)).when(folioModuleService).enable(any());
+    var moduleStageContext = new ModuleStageContext(new StageContextImpl("test",
+      Map.of(PARAM_REQUEST, EntitlementRequest.builder().build(), PARAM_MODULE_DESCRIPTOR, new ModuleDescriptor()),
+      Map.of()));
+    assertThatThrownBy(() -> folioModuleUpdater.execute(moduleStageContext)).isInstanceOf(IntegrationException.class);
+    verify(folioModuleService, times(3)).enable(any());
+  }
+
+  @Test
+  void testRetryOnUninstallerExecute() {
+    doThrow(new IntegrationException("Test", List.of(), 400)).when(folioModuleService).disable(any());
+    var moduleStageContext = new ModuleStageContext(new StageContextImpl("test",
+      Map.of(PARAM_REQUEST, EntitlementRequest.builder().build(), PARAM_MODULE_DESCRIPTOR, new ModuleDescriptor()),
+      Map.of()));
+    assertThatThrownBy(() -> folioModuleUninstaller.execute(moduleStageContext)).isInstanceOf(
+      IntegrationException.class);
+    verify(folioModuleService, times(3)).disable(any());
+  }
+
+  @Configuration
+  public static class TestConfig {
+
+    private RetryConfigProps quickBackoff() {
+      return RetryConfigProps.builder()
+        .backoff(RetryBackoffConfigProps.builder().delay(1).maxdelay(1).multiplier(1).build()).build();
+    }
+
+    @Bean
+    public RetryConfigurationProperties retryConfigurationProperties() {
+      return RetryConfigurationProperties.builder().module(quickBackoff()).build();
+    }
+
+    @Bean
+    public FolioModuleService folioModuleService() {
+      return mock(FolioModuleService.class);
+    }
+
+    @Bean
+    public FlowStageRepository flowStageRepository() {
+      return mock(FlowStageRepository.class);
+    }
+
+    @Bean
+    public EntitlementModuleService moduleService() {
+      return mock(EntitlementModuleService.class);
+    }
+
+    @Bean
+    @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public FolioModuleInstaller folioModuleInstaller(FolioModuleService folioModuleService) {
+      return new FolioModuleInstaller(folioModuleService);
+    }
+
+    @Bean
+    @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public FolioModuleUninstaller folioModuleUninstaller(FolioModuleService folioModuleService) {
+      return new FolioModuleUninstaller(folioModuleService);
+    }
+
+    @Bean
+    @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public FolioModuleUpdater folioModuleUpdater(FolioModuleService folioModuleService,
+      EntitlementModuleService moduleService) {
+      return new FolioModuleUpdater(folioModuleService, moduleService);
+    }
+  }
+}
