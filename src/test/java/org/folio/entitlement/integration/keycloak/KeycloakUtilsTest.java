d010d2175ff8f34142798f4d020c713035d625ea
vgema <viktor_gema@epam.com>
2024-12-13 15:10:44 +0100

    MODROLESKC-233: added logic to create resources for endpoints that do not exist in the mod-pubsub module but are still required to be created in the keycloak

--- src/test/java/org/folio/entitlement/integration/keycloak/KeycloakUtilsTest.java
+++ src/test/java/org/folio/entitlement/integration/keycloak/KeycloakUtilsTest.java
@@ -10,7 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @UnitTest
 @ExtendWith(MockitoExtension.class)
-public class KeycloakUtilsTest {
+class KeycloakUtilsTest {
   @Test
   void testUpdatePubSubDescriptorWithEndpoints() {
     ModuleDescriptor newDescriptor = new ModuleDescriptor();
