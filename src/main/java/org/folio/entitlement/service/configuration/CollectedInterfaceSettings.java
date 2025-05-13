b1fd9423ffff8487797834c65e46df11e36f1fef
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2025-05-13 18:48:26 +0300

    sonar issue fixed

--- src/main/java/org/folio/entitlement/service/configuration/CollectedInterfaceSettings.java
+++ src/main/java/org/folio/entitlement/service/configuration/CollectedInterfaceSettings.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class CollectedInterfaceSettings {
 
-  private static boolean DEFAULT_EXCLUDE_ENTITLED = true;
+  private static final boolean DEFAULT_EXCLUDE_ENTITLED = true;
 
   private boolean excludeEntitled = DEFAULT_EXCLUDE_ENTITLED;
 }
